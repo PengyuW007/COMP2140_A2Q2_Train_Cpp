c78b6913790e01b52633ba6241cc61e6c2a77d0a
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2021-12-24 21:40:50 -0600

    Update Train.h

--- Train.h
+++ Train.h
@@ -4,11 +4,24 @@
 #pragma once
 
 #include "TrainCar.h"
-
+#include <string>
 class Train {
 private:
     TrainCar car;
-    
+
+public:
+    Train();
+
+    ~Train();
+
+    void printInfo();//Print one line displaying the number of cars
+    //in the train and the total value of the cargo. On a second line, print the train from front
+    //to back, listing the type of cargo in each car (the list of cars may wrap to multiple lines).
+    void dropLast(int num);
+
+    void dropFirst(int num);
+
+    void drop(string type, int num);
 };
 
 #endif //TRAINCPP_TRAIN_H
